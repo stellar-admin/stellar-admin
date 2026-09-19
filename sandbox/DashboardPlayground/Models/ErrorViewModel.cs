eb19051c64934d8b5f1f9578c8102a6ea3c0759f
Jerrie Pelser <jerrie@jerriepelser.com>
2026-09-19 19:31:14 +0700

    Add DashboardPlayground

---
+++ sandbox/DashboardPlayground/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DashboardPlayground.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
